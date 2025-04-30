99dd8111730007ffe1ef0c4170e5e78543945219
lotteals <s204297@student.dtu.dk>
2025-04-30 08:59:52 +0200

    push

--- 6_dynamic.py
+++ 6_dynamic.py
@@ -8,10 +8,6 @@ import matplotlib.pyplot as plt
 from functools import partial
 import os
 
-LOAD_DIR = ''
-MAX_ITER = 0
-ABS_TOL = 0.0
-
 def load_data(load_dir, bid):
     SIZE = 512
     u = np.zeros((SIZE + 2, SIZE + 2))
@@ -82,26 +78,37 @@ def dynamic_speedup(n_proc_vector, building_ids):
 
 
 if __name__ == '__main__':
+    # Load data
     LOAD_DIR = '/dtu/projects/02613_2025/data/modified_swiss_dwellings/'
-    MAX_ITER = 20000
-    ABS_TOL = 1e-4
-
     with open(join(LOAD_DIR, 'building_ids.txt'), 'r') as f:
         building_ids = f.read().splitlines()
 
-    N = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    if len(sys.argv) < 2:
+        N = 1
+    else:
+        N = int(sys.argv[1])
     building_ids = building_ids[:N]
 
-    if len(sys.argv) > 2:
-        n_proc_vector = [int(arg) for arg in sys.argv[2:]]
-    else:
-        n_proc_vector = [1]
+    # Load floor plans
+    all_u0 = np.empty((N, 514, 514))
+    all_interior_mask = np.empty((N, 512, 512), dtype='bool')
+    for i, bid in enumerate(building_ids):
+        u0, interior_mask = load_data(LOAD_DIR, bid)
+        all_u0[i] = u0
+        all_interior_mask[i] = interior_mask
 
-    speed_ups, all_results = dynamic_speedup(n_proc_vector, building_ids)
+    # Run jacobi iterations for each floor plan
+    MAX_ITER = 20_000
+    ABS_TOL = 1e-4
 
-    # Print stats for first run
+    all_u = np.empty_like(all_u0)
+    for i, (u0, interior_mask) in enumerate(zip(all_u0, all_interior_mask)):
+        u = jacobi(u0, interior_mask, MAX_ITER, ABS_TOL)
+        all_u[i] = u
+
+    # Print summary statistics in CSV format
     stat_keys = ['mean_temp', 'std_temp', 'pct_above_18', 'pct_below_15']
-    print("\nbuilding_id, " + ', '.join(stat_keys))
-    for bid, stats in all_results[0]:
+    print('building_id, ' + ', '.join(stat_keys))  # CSV header
+    for bid, u, interior_mask in zip(building_ids, all_u, all_interior_mask):
+        stats = summary_stats(u, interior_mask)
         print(f"{bid},", ", ".join(str(stats[k]) for k in stat_keys))
-
