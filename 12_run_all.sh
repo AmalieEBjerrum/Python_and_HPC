9ca2184046730f3aff8f0b16d07b5ded9b5c30ed
lotteals <s204297@student.dtu.dk>
2025-04-30 12:13:43 +0200

    request enough memory

--- 12_run_all.sh
+++ 12_run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#BSUB -q gpua100
+#BSUB -q c02613
 #BSUB -J 12_run_all
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "rusage[mem=2GB]"
 #BSUB -gpu "num=1:mode=exclusive_process" 
 #BSUB -W 04:00 
 #BSUB -o 12_run_all.out
--- 3_visualization.sh
+++ 3_visualization.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#BSUB -J Task3
+#BSUB -J 3_visualization
 #BSUB -q hpc
 #BSUB -W 0:10
 #BSUB -R "rusage[mem=10GB]"
 #BSUB -R "select[model==XeonGold6226R]"
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
-#BSUB -o Task3.out
-#BSUB -e Task3.err
+#BSUB -o 3_visualization.out
+#BSUB -e 3_visualization.err
 
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
